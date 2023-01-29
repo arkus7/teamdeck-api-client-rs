137ea3b80f33a3a6d867c79d56828687bf126dad
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2023-01-29 16:06:04 +0100

    fix: make vacation reasons mod publicly visible

--- src/api/vacations/mod.rs
+++ src/api/vacations/mod.rs
@@ -5,4 +5,5 @@ mod single;
 
 pub use many::{Vacations, VacationsBuilder, VacationsBuilderError, VacationsSortBy};
 pub use periods::*;
+pub use reasons::*;
 pub use single::{Vacation, VacationBuilder, VacationBuilderError};
